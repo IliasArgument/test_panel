36105104e56ac732566cb1eff8196ca310491ad0
Ilia Koliesnikov <63620173+IliasArgument@users.noreply.github.com>
2021-03-22 11:28:10 +0200

    four

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -5,43 +5,29 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import "./Cards.css";
 
-const useStyles = makeStyles({
-
-  title: {
-    fontSize: 14,
-  },
-
-  item: {
-    display: "flex",
-    alignItems: "baseline",
-  },
-});
-
 const Cards = ({ items, count }) => {
-  const classes = useStyles();
-
   return (
     <div className="Cards">
       <Card className="Card">
         <CardContent>
-          <Typography
-            className={classes.title}
-            color="textSecondary"
-            gutterBottom
-          >
+          <Typography className="title" color="textSecondary" gutterBottom>
             Карта {count + 1}
           </Typography>
           <Typography variant="h5" component="h2" className="Cards_item">
-            <span className="item_title">Name:</span> <span className="item"> {items.name}</span>
+            <span className="item_title">Name:</span>{" "}
+            <span className="item"> {items.name}</span>
           </Typography>
           <Typography variant="h5" component="h2" className="Cards_item">
-          <span className="item_title">Last Name:</span> <span className="item"> {items.lastname}</span>
+            <span className="item_title">Last Name:</span>{" "}
+            <span className="item"> {items.lastname}</span>
           </Typography>
           <Typography variant="h5" component="h2" className="Cards_item">
-          <span className="item_title">Age:</span> <span className="item"> {items.age}</span>
+            <span className="item_title">Age:</span>{" "}
+            <span className="item"> {items.age}</span>
           </Typography>
           <Typography variant="h5" component="h2" className="Cards_item">
-          <span className="item_title">Sex:</span> <span className="item"> {items.sex}</span>
+            <span className="item_title">Sex:</span>{" "}
+            <span className="item"> {items.sex}</span>
           </Typography>
         </CardContent>
       </Card>
